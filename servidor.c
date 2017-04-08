2f709fddcbbb7b543ea4c175e76c340bda9bd466
Joaquin "Florius" Azcarate <j@florius.com.ar>
2017-04-08 18:48:25 -0300

    Loa acentos son malos!
    https://github.com/sisoputnfrba/foro/issues/630

--- servidor.c
+++ servidor.c
@@ -5,17 +5,17 @@
 #include <sys/socket.h>
 
 int main(void) {
-	struct sockaddr_in direcciónServidor;
-	direcciónServidor.sin_family = AF_INET;
-	direcciónServidor.sin_addr.s_addr = INADDR_ANY;
-	direcciónServidor.sin_port = htons(8080);
+	struct sockaddr_in direccionServidor;
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = INADDR_ANY;
+	direccionServidor.sin_port = htons(8080);
 
 	int servidor = socket(AF_INET, SOCK_STREAM, 0);
 
 	int activado = 1;
 	setsockopt(servidor, SOL_SOCKET, SO_REUSEADDR, &activado, sizeof(activado));
 
-	if (bind(servidor, (void*) &direcciónServidor, sizeof(direcciónServidor)) != 0) {
+	if (bind(servidor, (void*) &direcciónServidor, sizeof(direccionServidor)) != 0) {
 		perror("Falló el bind");
 		return 1;
 	}
